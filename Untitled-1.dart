d733e82f0ee786dfa27627704d8212eb50835c97
BARAIYA BALDEVSINH B. <baldevbaraiya18@gmail.com>
2026-02-23 18:09:45 +0530

    Add files via upload

---
+++ Untitled-1.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("View Products"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProductScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class ProductScreen extends StatelessWidget {
+
+  final List<String> products = ["Laptop", "Mobile", "Tablet"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Product List")),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index]),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(product: products[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Detail Screen
+class DetailScreen extends StatelessWidget {
+
+  final String product;
+
+  DetailScreen({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Details")),
+      body: Center(
+        child: Text("Product Name: $product", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
